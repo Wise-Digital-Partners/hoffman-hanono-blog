ac61ad3a08c4e9d468139f1b91207db5d4f48b88
hamletavetisian <55106549+hamletavetisian@users.noreply.github.com>
2022-03-09 17:35:43 -0800

    Update Footer.js

--- web/src/components/Footer/Footer.js
+++ web/src/components/Footer/Footer.js
@@ -150,7 +150,7 @@ const Footer = () => {
           <div className="flex flex-wrap md:flex-no-wrap flex-col md:flex-row items-center justify-center md:justify-between">
             <ul className="flex items-center flex-col md:flex-row md:space-x-10">
               <li className="text-small mb-1 md:mb-0">
-                © {getYear()} Hoffman Honono
+                © {getYear()} Hoffman Hanono
               </li>
               <div className="flex md:space-x-10">
                 <li className="text-small mb-1 md:mb-0">
