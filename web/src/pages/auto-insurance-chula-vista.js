bf98b0a2e4606ec9d2e3c4291c0438d96e93f70f
Anibal Rojas <anibal@wisedigitalpartners.com>
2025-04-03 18:13:04 -0500

    feat(frontend): Rework of Auto Insurance Page: https://app.clickup.com/t/86dw64m6z

--- web/src/pages/auto-insurance-chula-vista.js
+++ web/src/pages/auto-insurance-chula-vista.js
@@ -2,19 +2,23 @@ import React from "react";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 
+// import ButtonGhost from "../components/Button/ButtonGhost"
+import About from "../components/Repeating/About";
+import ButtonSolid from "../components/Button/ButtonSolid";
+import ButtonWithIcon from "../components/Button/ButtonWithIcon";
+import CallToAction from "../components/Repeating/CallToAction";
+import HeroAlignEnd from "../components/Hero/HeroAlignEnd";
+import HeroSplit from "../components/Hero/HeroSplit";
 import Layout from "../components/Layout";
 import SearchEngineOptimization from "../components/SEO";
-import HeroSplit from "../components/Hero/HeroSplit";
-import WhyUs from "../components/Repeating/WhyUs";
 import SliderTestimonials from "../components/Slider/SliderTestimonials";
-import About from "../components/Repeating/About";
-import CallToAction from "../components/Repeating/CallToAction";
+import WhyUs from "../components/Repeating/WhyUs";
 
 const Page = ({ data }) => {
-  const heroImages = [
-    data.heroDesktop.childImageSharp.fixed,
+  const newHeroImages = [
+    data.newHeroDesktop.childImageSharp.fixed,
     {
-      ...data.heroMobile.childImageSharp.fixed,
+      ...data.newHeroMobile.childImageSharp.fixed,
       media: `(max-width: 767px)`,
     },
   ];
@@ -27,32 +31,50 @@ const Page = ({ data }) => {
         openGraphImage={data.openGraphImage.publicURL}
         twitterOpenGraphImage={data.twitterOpenGraphImage.publicURL}
       />
-
-      <HeroSplit
-        images={heroImages}
+      <HeroAlignEnd
+        images={newHeroImages}
         position="50% 50%"
-        maxHeightDesktop="618px"
-        maxHeightMobile="500px"
+        maxHeightDesktop="600px"
+        maxHeightMobile="580px"
       >
         <div className="max-w-2xl">
-          <h1>Chula Vista Auto Insurance</h1>
-          <p className="mb-0">
-            Whether your car is parked or on the move, make sure you’re covered
-            at all times. While we try to keep our vehicles safe, there’s no
-            guarantee that others around us will do the same. At Hoffman Hanono,
-            we match you with the right auto insurance that goes beyond just
-            accident coverage.
+          <p className="text-white font-heading text-mobile6xl md:text-6xl leading-tight font-bold mb-6">
+            Beyond Coverage
           </p>
+          <p className="text-white font-display text-mobile4xl md:text-2xl leading-tight mb-10">
+            Your Peace of Mind is Our Business
+          </p>
+          <ButtonSolid data="modal-quote" text="Get a Quote" />
         </div>
-      </HeroSplit>
+      </HeroAlignEnd>
+
+      <section className="mt-16 mb-12 md:mt-32 md:mb-24">
+        <div className="container">
+          <p className="decorative-text">Established in 1959</p>
+          <div className="grid grid-cols-1 md:grid-cols-2 md:gap-24">
+            <div>
+              <h1>Chula Vista Auto Insurance</h1>
+            </div>
+            <div>
+              <p className="mb-0">
+                Whether your car is parked or on the move, make sure you’re
+                covered at all times. While we try to keep our vehicles safe,
+                there’s no guarantee that others around us will do the same. At
+                Hoffman Hanono, we match you with the right auto insurance that
+                goes beyond just accident coverage.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
 
       <section className="wrapper">
         <div className="container">
-          <div className="grid grid-cols-1 md:grid-cols-12 gap-y-10 md:gap-y-0 md:gap-x-24 items-center">
-            <div className="md:col-start-1 md:col-end-7">
-              <Img fluid={data.whatsIncluded.childImageSharp.fluid} />
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-y-8 md:gap-y-0 items-center">
+            <div className="md:col-start-1 md:col-end-8">
+              <Img fluid={data.introDesktop.childImageSharp.fluid} />
             </div>
-            <div className="md:col-end-13 md:col-span-6">
+            <div className="md:col-end-13 md:col-span-4">
               <h2>The Best Premiums for Your Needs</h2>
               <p>
                 Account for all risks and choose the most comprehensive coverage
@@ -67,37 +89,192 @@ const Page = ({ data }) => {
                 <li>Collision coverage</li>
                 <li>Comprehensive coverage for vehicle theft or vandalism</li>
               </ul>
+              {/* <ButtonWithIcon
+                href="/business-insurance-chula-vista/"
+                text={[
+                  "View Products",
+                  <i className="far fa-arrow-right ml-2"></i>,
+                ]}
+              /> */}
             </div>
           </div>
         </div>
       </section>
 
-      <div className="container">
-        <section className="wrapper bg-gray-100 pt-24 pb-20 md:py-32 px-6 rounded-xl md:rounded-3xl">
-          <div className="max-w-4xl mx-auto">
-            <p className="decorative-text">Custom Solutions</p>
-            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-6">
-              <div>
-                <h2>Our Auto Insurance Approach</h2>
-              </div>
-              <div>
-                <p className="mb-0">
-                  Whether you are managing risk for business or personal assets,
-                  Hoffman Hanono Insurance Services is here to advise and
-                  partner with you to find insurance solutions that are meant
-                  entirely for you. After all, insurance isn’t
-                  “one-size-fits-all.” We’ll only place you where you belong.
-                  Not only that, but we provide quality service and resources
-                  that will help you secure better insurance premiums in the
-                  long run. This commitment reveals why we've been a pillar of
-                  the San Diego and Chula Vista insurance community since 1959.
-                </p>
-              </div>
+      <section className="wrapper">
+        <div className="container">
+          <div className="grid grid-cols-1 md:grid-cols-12 gap-y-8 md:gap-y-0 items-center">
+            <div className="md:col-start-1 md:col-end-5 row-start-2 md:row-start-1">
+              <h2>The Best Premiums for Your Needs</h2>
+              <p>
+                Account for all risks and choose the most comprehensive coverage
+                for you, your vehicle and your passengers. Our partner carriers
+                provide the following:
+              </p>
+              <ul className="styled-list">
+                <li>
+                  Liability coverage for bodily injury and property damage
+                </li>
+                <li>Personal injury protection</li>
+                <li>Collision coverage</li>
+                <li>Comprehensive coverage for vehicle theft or vandalism</li>
+              </ul>
+              {/* <ButtonWithIcon
+                href="/personal-insurance-agency/"
+                text={[
+                  "View Products",
+                  <i className="far fa-arrow-right ml-2"></i>,
+                ]}
+              /> */}
+            </div>
+            <div className="md:col-span-7 md:col-end-13 row-start-1">
+              <Img fluid={data.whatsIncluded.childImageSharp.fluid} />
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
 
+      <section className="wrapper">
+        <div className="container">
+          <div className="grid grid-cols-1 gap-y-8 md:gap-y-0 items-center">
+            <div className="">
+              <h2>Comprehensive Auto Insurance Coverage in Chula Vista</h2>
+              <p>
+                Finding the right auto insurance in Chula Vista isn’t just about
+                meeting California’s minimum requirements—it’s about
+                personalizing your policy to fit your lifestyle, driving habits,
+                and personal risk tolerance. At Hoffman & Hanono, we offer auto
+                insurance packages for Chula Vista drivers who need policies
+                tailored to their exact needs:
+              </p>
+              <h3>Liability Insurance</h3>
+              <p>
+                Required by California law, liability insurance protects your
+                finances if you cause bodily injury or property damage. You
+                should know that as of January 1, 2025, California’s minimum
+                liability limits for auto insurance policies have increased:
+              </p>
+              <ul className="styled-list">
+                <li>
+                  $30,000 for bodily injury or death per person (up from
+                  $15,000)
+                </li>
+                <li>
+                  $60,000 for bodily injury or death per accident (up from
+                  $30,000)
+                </li>
+                <li>
+                  $15,000 for property damage per accident (up from $5,000)
+                </li>
+              </ul>
+              <p>
+                This means that if you’re carrying the old minimum coverage
+                limits, you’ll need to update your policy to comply with the new
+                California law.
+              </p>
+              <p>
+                While these increased limits provide more robust protection,
+                many Chula Vista auto insurance policyholders need increased
+                protection.
+              </p>
+              <h3>Personal Injury Protection (MedPay)</h3>
+              <p>
+                In other states, drivers may carry personal injury protection
+                (PIP) insurance, a “no-fault” policy that protects policyholders
+                regardless of who causes the injury or property damage. While
+                PIP insurance is not available in “at-fault” states like
+                California, Chula Vista car insurance policyholders may choose
+                to carry MedPay insurance, an optional policy that:
+              </p>
+              <ul className="styled-list">
+                <li>
+                  Covers medical bills and funeral expenses for you and
+                  passengers, regardless of fault
+                </li>
+                <li>
+                  Provides coverage if you are injured as a pedestrian,
+                  passenger, or while using public transportation
+                </li>
+                <li>Pays out quickly, without a deductible or copay</li>
+              </ul>
+              <p>
+                MedPay can be a smart investment, particularly for drivers who
+                do not have health insurance coverage or need more robust
+                protection.
+              </p>
+              <h3>Collision Coverage</h3>
+              <p>
+                Limited liability insurance covers damage to others, but it does
+                not pay for repairs to your vehicle. This is where collision
+                coverage kicks in.
+              </p>
+              <p>
+                In California, collision coverage is optional—unless you are
+                financing or leasing a vehicle. In such cases, most lenders
+                require you to carry it. Many Chula Vista auto insurance
+                policyholders choose collision coverage for peace of mind,
+                knowing they are protected against costly repairs after an
+                accident.
+              </p>
+              <h3>Comprehensive Coverage</h3>
+              <p>
+                Comprehensive coverage protects Chula Vista car insurance
+                policyholders against unexpected events—whether it's theft, a
+                fallen tree, or damage caused by a weather-related event. If you
+                lease or finance your vehicle, lenders will likely require you
+                to carry comprehensive coverage.
+              </p>
+              <p>
+                However, many Chula Vista car insurance policyholders opt for
+                comprehensive coverage even if their vehicle is paid off. Not
+                only does it provide peace of mind, but it also protects your
+                investment when life throws you an unexpected challenge.
+              </p>
+              <h2>Finding the Right Auto Insurance in Chula Vista, CA</h2>
+              <p>
+                Every driver in Chula Vista has unique auto insurance needs. If
+                you’re unsure which coverage options best suit your lifestyle
+                and risk tolerance, consider the following:
+              </p>
+              <ul className="styled-list">
+                <li className="!inline-block">
+                  <strong>Daily commuters:</strong> If you travel Chula Vista's
+                  busy roads and highways every day, consider higher liability
+                  limits beyond California requirements. That, along with
+                  comprehensive and collision coverage, gives you peace of mind,
+                  knowing you're protected from unexpected events.
+                </li>
+                <li className="!inline-block">
+                  <strong>Remote workers:</strong> If you spend most of the week
+                  at home, you may qualify for low-mileage discounts on your
+                  auto insurance in Chula Vista. However, it is still important
+                  to maintain adequate liability coverage, even if you are an
+                  occasional driver.
+                </li>
+                <li className="!inline-block">
+                  <strong>Families with new drivers:</strong> Whether you are a
+                  new driver or have a child who is learning to drive, we
+                  recommend increasing limits on basic liability auto insurance
+                  in Chula Vista. Also, consider combining basic coverage with
+                  collision and MedPay to maximize protection.
+                </li>
+                <li className="!inline-block">
+                  <strong>Luxury vehicle drivers:</strong> Protect your valuable
+                  investment with comprehensive and collision coverage to
+                  maximize protection against theft, vandalism, and accidents.
+                </li>
+                <li className="!inline-block">
+                  <strong>Budget-conscious drivers:</strong> While you are
+                  legally required to carry limited liability auto insurance in
+                  Chula Vista, our agents will work with you to balance
+                  liability limits with your budget—without compromising your
+                  essential protection.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
       <WhyUs />
       <SliderTestimonials />
       <About />
@@ -120,10 +297,8 @@ export const query = graphql`
     ) {
       publicURL
     }
-    heroDesktop: file(
-      relativePath: {
-        eq: "personal-insurance/auto-insurance/auto-hero-desktop.jpg"
-      }
+    newHeroDesktop: file(
+      relativePath: { eq: "personal-insurance/auto-insurance/hero-desktop.jpg" }
     ) {
       childImageSharp {
         fixed(width: 548, height: 618) {
@@ -131,14 +306,32 @@ export const query = graphql`
         }
       }
     }
-    heroMobile: file(
-      relativePath: {
-        eq: "personal-insurance/auto-insurance/auto-hero-mobile.jpg"
-      }
+    newHeroMobile: file(
+      relativePath: { eq: "personal-insurance/auto-insurance/hero-mobile.jpg" }
     ) {
       childImageSharp {
         fixed(width: 748, height: 748) {
           ...GatsbyImageSharpFixed_withWebp
+        }
+      }
+    }
+    personalInsurance: file(
+      relativePath: { eq: "home/personal-insurance.jpg" }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 1392) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    introDesktop: file(
+      relativePath: {
+        eq: "personal-insurance/auto-insurance/auto-hero-desktop.jpg"
+      }
+    ) {
+      childImageSharp {
+        fluid(maxWidth: 1392) {
+          ...GatsbyImageSharpFluid_withWebp
         }
       }
     }
